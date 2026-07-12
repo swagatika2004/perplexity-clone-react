3ae8d072b3b4b7d07aa2d19e91bfe48592eb19fd
swagatika2004 <157866910+swagatika2004@users.noreply.github.com>
2026-07-12 15:22:14 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-// import Sidebar from "./components/Sidebar";
-// import Navbar from "./components/Navbar";
-// import Search from "./components/Search";
-// import Discover from "./components/Discover";
-// import Footer from "./components/Footer";
-// function App() {
-//   return (
-//     <>
-//       <Sidebar />
-//       <Navbar/>
-//       <Search/>
-//       <Discover/>
-//       <Footer/>
-//     </>
-//   );
-// }
-
-// export default App;
 
 import "./App.css";
 
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
 import Search from "./components/Search";
-import Discover from "./components/Discover"
+import Discover from "./components/Discover";
 import Footer from "./components/Footer";
 
 function App() {
@@ -42,21 +23,6 @@ function App() {
         <Footer />
       </div>
     </div>
-=======
-import Discover from "./components/Discover";
-
-function App() {
-  return (
-    <>
-      
-      <Sidebar />
-      <Footer/>
-      <Navbar/>
-      <Search/>
-      <Discover />
-      
-    </>
->>>>>>> 764d4720b93a6453ad5bf20dbb5ad45590a5a9cb
   );
 }
 
